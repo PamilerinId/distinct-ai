a745d9f9ba92fd407cff3dc2386a27ef3ffcac12
Pamilerin Idowu <pamilerinid@gmail.com>
2023-02-02 14:56:43 +0100

    resolve form data

--- frontend/src/services/file-upload.service.js
+++ frontend/src/services/file-upload.service.js
@@ -5,18 +5,13 @@ import * as axios from 'axios';
 const BASE_URL = 'http://localhost:5000';
 
 function upload(formData) {
-  axios({
-    url: BASE_URL,
-    method: 'GET',
-    responseType: 'blob', // important
-  }).then((response) => {
-    const url = window.URL.createObjectURL(new Blob([response.data]));
-    const link = document.createElement('a');
-    link.href = url;
-    link.setAttribute('download', 'file.pdf');
-    document.body.appendChild(link);
-    link.click();
-  });
+  const url = `${BASE_URL}/generate-invoice`;
+  return axios.post(url, formData)
+  // get data
+    .then((x) => x.data);
+  // add url field
+  // .then(x => x.map(img => Object.assign({},
+  //     img, { url: `${BASE_URL}/images/${img.id}` })));
 }
 
 export default upload;
